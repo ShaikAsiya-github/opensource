7b9463c92f03c17f6dc71a4081f52d8cd1a4265d
ShaikAsiya-github <2003shaikasiya@gmail.com>
2024-11-19 21:56:51 +0530

    Create TransposeMatrix.py

---
+++ visaa_prep/TransposeMatrix.py
@@ -0,0 +1,5 @@
+N = int(input())
+matrix = [list(map(int, input().split())) for _ in range(N)]
+transpose = [[matrix[j][i] for j in range(N)] for i in range(N)]
+for row in transpose:
+    print(" ".join(map(str, row)))
